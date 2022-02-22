d2d0e64c7af98ddb6e1fd12fca3da30009ce0dba
Zach Killebrew <zbkilleb@gmail.com>
2022-02-22 11:33:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# mass-install
+
+## A package for recursively detecting package.json files and installing dependencies
+
+For use in monorepos or projects with multiple package.json files. `mass-install` will navigate the project structure and install dependencies for each specific directory.
+
+## Running the application
+
+### `npm install mass-install`
+
+Run the command `mass-install` to begin dependency installation.
+
+If a `.gitignore` file is detected in the root of the project, the `mass-install` script will ask the user if they want to add the newly generated `node_modules` directories to it. These changes will be appended to the bottom of the `.gitignore` file.
+
+Example Formatting:
+
+```
+# Automatically added by mass-install
+/backend/node_modules
+/src/node_modules
+```
+
+The `.gitignore` prompt can be bypassed by running the command as `mass-install --no-update`.
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const currentDir = './';
+const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
+const readline = require('readline');
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+let packagePaths = [];
+
+function readDirectory(directory) {
+    try {
+        fs.readdirSync(directory).forEach((file) => {
+            if(directory !== 'node_modules' && directory !== '.git' && file !== 'node_modules' && file !== '.git') {
+                let fullPath = path.join(directory, file);
+                if(file === 'package.json') {
+                    packagePaths.push(directory);
+                    console.log(`Found package.json at ${fullPath}. Installing dependencies...`);
+                    child_process.exec('npm install', { cwd: directory }, () => {
+                        console.log(`Install complete for ${fullPath}.`);
+                    });
+                }
+                if(fs.lstatSync(fullPath).isDirectory()) {
+                    readDirectory(fullPath);
+                }
+            }
+        });
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+function askPrompt() {
+    rl.question('We found a .gitignore. Do you want to add the new node_modules directories to it? (Y/N): ', (answer)  => {
+        if(answer === 'Y' || answer === 'y') {
+            rl.close();
+            updateGitignore();
+        }
+        else if (answer !== 'N' && answer !== 'n') askPrompt();
+        else rl.close();
+    });
+}
+
+function updateGitignore() {
+    let writeString = '\n# Automatically added by mass-install\n';;
+    try {
+        packagePaths.forEach((path) => {
+            writeString += `/${path}/node_modules\n`
+        });
+        fs.appendFileSync(`${currentDir}/.gitignore`, writeString);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function main() {
+    readDirectory(currentDir);
+    if(fs.existsSync('.gitignore') && !process.argv.includes('--no-update')) {
+        askPrompt();
+    }
+}
+
+main();
